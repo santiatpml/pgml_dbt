3cf42cab95a068d2a1e12383407e19de2a594e5a
Santi Adavani <santi@hyperparam.ai>
2023-06-28 21:56:08 -0700

    added embeddings and transforms

--- models/chunks.sql
+++ models/chunks.sql
@@ -1,6 +1,7 @@
 {{
     config(
-        materialized='incremental'
+        materialized='incremental',
+        unique_key=['document_id', 'splitter_id', 'chunk']
     )
 }}
 WITH document as (
@@ -9,8 +10,8 @@ WITH document as (
 ),
 splitter as (
 	select id, name, parameters
-	from {{ source('test_collection_1', 'splitters') }}
-	where name = '{{ var('splitter_name')}}' and parameters = '{{ tojson(var('splitter_parameters'))}}'::jsonb
+	from {{ ref('splitters') }}
+	where name = '{{ var('splitter_name')}}' and parameters @> '{{ tojson(var('splitter_parameters'))}}'::jsonb and parameters <@ '{{ tojson(var('splitter_parameters'))}}'::jsonb
 ),
 chunks as (
     select document.id document_id, splitter.id splitter_id, chunk_index, chunk from document, splitter, pgml.chunk(splitter.name, document.text, splitter.parameters)
--- models/embeddings.sql
+++ models/embeddings.sql
@@ -1,18 +1,24 @@
+{% set alias_name = var('task') + '_' + local_md5(var('model_name') + 
+					tojson(var('model_parameters',{})) + 
+					var('splitter_name') + tojson(var('splitter_parameters',{})))[:5]
+%}
 {{
     config(
-        materialized='incremental'
+        materialized='incremental',
+		unique_key=['document_id', 'splitter_id', 'chunk_index'],
+		alias = alias_name
     )
 }}
-WITH document as (
-	select id, text
-	from {{ source('test_collection_1', 'documents') }}
+WITH chunk as (
+	select document_id, splitter_id, chunk_index, chunk
+	from {{ ref('chunks') }}
 ),
-splitter as (
+model as (
 	select id, name, parameters
-	from {{ source('test_collection_1', 'splitters') }}
-	where name = '{{ var('splitter_name')}}' and parameters = '{{ tojson(var('splitter_parameters'))}}'::jsonb
+	from {{  ref('models') }}
+	where name = '{{ var('model_name')}}' and parameters @> '{{ tojson(var('model_parameters',{}))}}'::jsonb and parameters <@ '{{ tojson(var('model_parameters',{}))}}'::jsonb
 ),
-chunks as (
-    select document.id document_id, splitter.id splitter_id, chunk_index, chunk from document, splitter, pgml.chunk(splitter.name, document.text, splitter.parameters)
+embeddings as (
+    select chunk.document_id document_id, chunk.splitter_id splitter_id, chunk.chunk_index chunk_index, cast(embed as vector) embedding from chunk, model, pgml.embed(model.name, chunk.chunk, model.parameters)
 )
-select * from chunks
+select * from embeddings
